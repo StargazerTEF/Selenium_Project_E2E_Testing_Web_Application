08c2fdac441bc6951efb82cb72cf94ba00ac2adc
Marko Peric <markoperic986@gmail.com>
2023-10-31 14:28:13 +0100

    Add Basic test

---
+++ src/test/java/tests/BasicTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import helper.Helper;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public abstract class BasicTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl = "https://www.saucedemo.com/";
+    protected JavascriptExecutor js;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        js = (JavascriptExecutor) driver;
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.manage().deleteAllCookies();
+        driver.navigate().to(baseUrl);
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult testResult) throws IOException {
+        js.executeScript("window.localStorage.clear();");
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            Helper.takeScreenshotWithDateTimeFormat(driver, testResult.getName());
+        }
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
+
