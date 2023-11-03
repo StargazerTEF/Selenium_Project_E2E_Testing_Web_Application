9eb114a04dbc7e4509da8114f706be5aabce1f01
Marko Peric <markoperic986@gmail.com>
2023-11-03 11:40:18 +0100

    Add Test scenario: Checkout information - Test case: Verify that user can't checkout with all fields left blank

--- src/test/java/tests/CheckoutTests.java
+++ src/test/java/tests/CheckoutTests.java
@@ -408,4 +408,22 @@ public class CheckoutTests extends BasicTest {
         Assert.assertEquals(checkoutPage.getErrorMessageText(), "Error: First Name is required",
                 "Text of error message should be 'Error: First Name is required'.");
     }
+
+    @Test(priority = 33, retryAnalyzer = RetryAnalyzer.class)
+    public void VerifyThatUserCantCheckoutWithAllFieldsLeftBlank() {
+
+        String firstName = "";
+        String lastName = "";
+        String zipCode = "";
+
+        loginPage.loginWIthValidCredentials();
+        headerPage.clickOnCartIcon();
+        cartPage.clickOnCheckoutButton();
+        checkoutPage.fillInFirstNameField(firstName);
+        checkoutPage.fillInLastNameField(lastName);
+        checkoutPage.fillInZipCodeField(zipCode);
+        checkoutPage.clickOnContinueButton();
+        Assert.assertEquals(checkoutPage.getErrorMessageText(), "Error: First Name is required",
+                "Text of error message should be 'Error: First Name is required'.");
+    }
 }
