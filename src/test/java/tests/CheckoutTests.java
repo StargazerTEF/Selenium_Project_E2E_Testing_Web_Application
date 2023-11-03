e5511860c527363d4e636507b357b1ec64b95010
Marko Peric <markoperic986@gmail.com>
2023-11-03 11:47:32 +0100

    Add Test scenario: Checkout information - Test case: Verify that user can't checkout with 'Last Name' field containing numbers

--- src/test/java/tests/CheckoutTests.java
+++ src/test/java/tests/CheckoutTests.java
@@ -444,4 +444,22 @@ public class CheckoutTests extends BasicTest {
         Assert.assertEquals(checkoutPage.getErrorMessageText(), "Error: First Name is Invalid",
                 "Text of error message should be 'Error: First Name is Invalid'.");
     }
+
+    @Test(priority = 35, retryAnalyzer = RetryAnalyzer.class)
+    public void VerifyThatUserCantCheckoutWithLastNameFieldContainingNumbers() {
+
+        String firstName = "Aki";
+        String lastName = "123";
+        String zipCode = "11000";
+
+        loginPage.loginWIthValidCredentials();
+        headerPage.clickOnCartIcon();
+        cartPage.clickOnCheckoutButton();
+        checkoutPage.fillInFirstNameField(firstName);
+        checkoutPage.fillInLastNameField(lastName);
+        checkoutPage.fillInZipCodeField(zipCode);
+        checkoutPage.clickOnContinueButton();
+        Assert.assertEquals(checkoutPage.getErrorMessageText(), "Error: Last Name is Invalid",
+                "Text of error message should be 'Error: Last Name is Invalid'.");
+    }
 }
