1bd9ef005b8e96b0ece3b740a7913d9f357f0e95
Marko Peric <markoperic986@gmail.com>
2023-11-03 17:04:20 +0100

    Add Test scenario: Confirmation page - Test case: Verify the url

---
+++ src/test/java/tests/ConfirmationTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class ConfirmationTests extends BasicTest {
+
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void VerifyTheUrl() {
+
+        loginPage.loginWIthValidCredentials();
+        headerPage.clickOnCartIcon();
+        cartPage.clickOnCheckoutButton();
+        checkoutPage.fillOutCheckoutFormWithValidInputs();
+        checkoutPage.clickOnContinueButton();
+        paymentPage.clickOnFinishButton();
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "checkout-complete.html",
+                "Current url should be " + baseUrl + " checkout-complete.html");
+    }
+}
